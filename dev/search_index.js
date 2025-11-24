2a461f3b87fbaf9f9f886ed28e4688d9c8191873
Documenter.jl <documenter@juliadocs.github.io>
2025-11-24 13:34:52 +0000

    build based on 65f2a40

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#ITensorUnicodePlots","page":"Home","title":"ITensorUnicodePlots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ITensorUnicodePlots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#ITensorUnicodePlots","page":"Home","title":"ITensorUnicodePlots","text":"Documentation for ITensorUnicodePlots.\n\n","category":"section"}]
 }
